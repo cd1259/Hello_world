5321e82c6ada581c8e082f082700dbd0b39eaa23
Bill <92319486+cd1259@users.noreply.github.com>
2021-10-21 08:39:39 +0800

    Add files via upload

---
+++ 乘法口诀表.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i, k = 0, sum = 0;
+	for (i = 1, k = 1; i <= 9; i++)
+	{
+
+		for (k = 1; i >= k; k++)
+		{
+			sum = i * k;
+			printf("%d*%d=%d ", i, k, sum);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}

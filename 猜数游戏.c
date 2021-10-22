dbf7c42c728c76df2a00a100720f1a01867ea64b
Bill <92319486+cd1259@users.noreply.github.com>
2021-10-22 19:25:17 +0800

    Add files via upload

--- 猜数游戏.c
+++ 猜数游戏.c
@@ -2,9 +2,14 @@
 int main()
 {
 	//猜数游戏
-	int num = 123, guess;
+	printf("*****************************************\n");
+	printf("*************** 猜数游戏 ****************\n");
+	printf("*****************************************\n");
+	printf("***************** play ******************\n"); 
+	int num = 123;     //要猜的数，我还不会生成随机数呜呜呜
+	int guess, n = 1;
 	int left = 1, right = 200;
-	printf("请输入一个1~200的整数:>");
+	printf("\n请输入一个1~200的整数:>");
 	scanf_s("%d", &guess);
 	while (guess < 1 || guess>200)
 	{
@@ -16,8 +21,9 @@ int main()
 		if (guess < num)
 		{
 			left = guess + 1;
-			printf("没猜中，小了\n范围为%d~%d,再猜一遍:>", left, right);
+			printf("\n没猜中，小了\n范围为%d~%d,再猜一遍:>", left, right);
 			scanf_s("%d", &guess);
+			n++;
 			while (guess < left || guess>right)
 			{
 				printf("范围为%d~%d，请重新输入:>", left, right);
@@ -27,8 +33,9 @@ int main()
 		else if (guess > num)
 		{
 			right = guess - 1;
-			printf("没猜中，大了\n范围为%d~%d,再猜一遍:>", left, right);
+			printf("\n没猜中，大了\n范围为%d~%d,再猜一遍:>", left, right);
 			scanf_s("%d", &guess);
+			n++;
 			while (guess < left || guess>right)
 			{
 				printf("范围为%d~%d，请重新输入:>", left, right);
@@ -36,7 +43,8 @@ int main()
 			}
 		}
 	}
-	printf("猜中了，结果为%d\n", guess);
+	printf("\n猜中了，结果为%d\n", guess);
+	printf("总共猜了%d次\n", n);
 	
 	return 0;
 }

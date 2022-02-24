13da97e3a01ef718818d2deee5daff56a12d92ba
Bill <92319486+cd1259@users.noreply.github.com>
2022-02-24 09:51:07 +0800

    Add files via upload

---
+++ practice_everyday/2_24.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	/*char* a = "hello ";
+	char* c = a;
+	char* b = "world";
+	printf("%s\n%s\n", a, b);
+	strcat_s(a, b);
+	printf("%s\n%s\n", a, b);
+	printf("%d\n", strcmp(a, c));*/
+
+	/*char src[50], dest[50];
+
+	strcpy(src, "This is source");
+	strcpy(dest, "This is destination");
+
+	strcat(*dest, *src);
+
+	printf("最终的目标字符串： |%s|", dest);*/
+	return 0;
+}
